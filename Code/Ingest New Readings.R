043ec63902d79b6c636e8db6629f3767ada05f6a
AkthemRehab <akthem@gmail.com>
2021-05-31 20:32:41 +0200

    Added  a line to update/save init_file_list to match the file_list so the comparison is always with the most recent list of files

--- Code/Ingest New Readings.R
+++ Code/Ingest New Readings.R
@@ -1,12 +1,18 @@
+library(rlist)
+folder_name <- "Data test"
+setwd(folder_name)
+
+load("init_file_list.RData")
 file_list <- list.files(pattern = "*.*")
 
 if(!identical(setdiff(file_list, init_file_list),character(0))){
      temp <- setdiff(file_list, init_file_list)
-     test_1st <- parLapply(cl, temp, read.delim, header = F, colClasses = c("NULL", "NULL", "NULL", "NULL", "NULL", 
-                                                                            "NULL", NA, NA,))
+     test_1st <- lapply(temp, read.delim, header = F, colClasses = c("NULL", "NULL", "NULL", "NULL", "NULL", 
+                                                                     "NULL", NA, NA))
      ID = c(1:length(test_1st))
      test_1st_binded <- mapply(cbind, test_1st, "ID"=ID, SIMPLIFY=F)
      test_1st_binded <- list.rbind(test_1st_binded)
      init_file_list <- file_list
+     save(init_file_list, file = "init_file_list.RData")
 } else
      print("no new files found")
