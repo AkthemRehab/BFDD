558d42e79629d4b4912ae4edf403c66d691961be
AkthemRehab <akthem@gmail.com>
2021-05-31 21:58:24 +0200

    Now, We set the folder by the full path to avoid confusion when the script is not opened from the project.

--- Code/Initiate Listener.R
+++ Code/Initiate Listener.R
@@ -1,7 +1,6 @@
 #Initiate the listener
 
-folder_name <- "Data test"
-setwd(folder_name)
+setwd("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/Data test")
 
 init_file_list <- character(0)
 save(init_file_list, file = "init_file_list.RData")
