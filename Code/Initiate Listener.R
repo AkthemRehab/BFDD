b1836254a16f778386ddc8ea763dace687fa811f
AkthemRehab <akthem@gmail.com>
2021-05-31 20:17:49 +0200

    Updated the code to have the file in the Data test folder

--- Code/Initiate Listener.R
+++ Code/Initiate Listener.R
@@ -1,9 +1,7 @@
 #Initiate the listener
-init_file_list <- character(0)
-save(init_file_list, file = "init_file_list.RData")
-
 
 folder_name <- "Data test"
 setwd(folder_name)
 
-file_list <- list.files(pattern = "*.*")
+init_file_list <- character(0)
+save(init_file_list, file = "init_file_list.RData")
