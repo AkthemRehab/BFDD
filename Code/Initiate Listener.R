b8be6bd3af0f5dfa64de28ddac61161fc92577e5
AkthemRehab <akthem@gmail.com>
2021-06-01 13:06:16 +0200

    Added the Results variable to accoodate the accumulation of the results

--- Code/Initiate Listener.R
+++ Code/Initiate Listener.R
@@ -1,6 +1,5 @@
-#Initiate the listener
-
-setwd("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/Data test")
-
 init_file_list <- character(0)
+Results <- NULL
+setwd("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/init")
 save(init_file_list, file = "init_file_list.RData")
+write.csv(Results, file="Results.csv")
