7a209116488a92caf25656b1a8e279f3d88eaf97
AkthemRehab <akthem@gmail.com>
2021-06-05 00:20:50 +0200

    fixed init_file_list pinning to be init-file-list to accomodate pin changing _ to - in the name of the pin..

--- Code/1- Ingest New Readings.R
+++ Code/1- Ingest New Readings.R
@@ -1,9 +1,9 @@
 library(rlist)
 
-init_file_list <- pin_get("init_file_list", board = "GitHub-BFDD")
+init_file_list <- pin_get("init-file-list", board = "GitHub-BFDD")
 Results <- pin_get("Results", board = "GitHub-BFDD")
-setwd("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/Data test")
 
+setwd("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/Data test")
 file_list <- list.files(pattern = "*.*")
 
 if(!identical(setdiff(file_list, init_file_list),character(0))){
@@ -14,7 +14,7 @@ if(!identical(setdiff(file_list, init_file_list),character(0))){
      test_1st_binded <- mapply(cbind, test_1st, "ID"=ID, SIMPLIFY=F)
      test_1st_binded <- list.rbind(test_1st_binded)
      init_file_list <- file_list
-     pin(init_file_list, board = "GitHub-BFDD")
+     pin(init_file_list, "init-file-list",board = "GitHub-BFDD")
      source("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/Code/2- Transform - Overlapping Windows.R")
 } else
      print("no new files found")
