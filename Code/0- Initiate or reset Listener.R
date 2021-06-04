162394ef0f88627d5cccffa2d23319ed4025edb5
AkthemRehab <akthem@gmail.com>
2021-06-05 01:34:11 +0200

    changed the init_file_list to be a NULL object
    and changed the save to be a pin object
    
    moved the board registeration to script # 1 to allow for proper taskscheduleR

--- Code/0- Initiate or reset Listener.R
+++ Code/0- Initiate or reset Listener.R
@@ -1,19 +1,20 @@
 #check out the butcher package to streamline the model file
-#check out the pins package to cache and load the model
+
+#Creating the pins connection
+#Remember that we created the GITHUB_PAT first. check for its existence using below line
+nchar(Sys.getenv("GITHUB_PAT")) > 0
+
+#taskscheduleR
 #Create the task schedule job
 library(taskscheduleR)
 myscript <- ("D:/Documents/One Drive/OneDrive - Alexandria University/1- Ingest New Readings.R")
 taskscheduler_create(taskname = "Ingest-New-Readings", rscript = myscript,
-                     schedule = "MINUTE", starttime = "13:46", modifier = 1)
-
-#Check all tasks
-taskscheduler_ls()
-
+                     schedule = "MINUTE", starttime = "01:30", modifier = 5)
 #delete the task
 taskscheduler_delete(taskname = "Ingest-New-Readings")
 
-init_file_list <- character(0)
+init_file_list <- NULL
 Results <- data.frame(Results=numeric(), stringsAsFactors = F)
-setwd("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/init")
-save(init_file_list, file = "init_file_list.RData")
-save(Results, file="Results.RData")
+
+pin(init_file_list, "init-file-list",board = "GitHub-BFDD")
+pin(Results, "Results",board = "GitHub-BFDD")
