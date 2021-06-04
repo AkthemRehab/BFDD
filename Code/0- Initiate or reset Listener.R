0f5151c3ad2b23500f1302adbf0d5326ffaca9fe
AkthemRehab <akthem@gmail.com>
2021-06-04 17:07:51 +0200

    Multiple changes. Changed results to be an empty data frame instead of csv. Moved u and sd to 5th rscript. Moved model to 5th rscript. others too

--- Code/0- Initiate or reset Listener.R
+++ Code/0- Initiate or reset Listener.R
@@ -1,13 +1,19 @@
 #check out the butcher package to streamline the model file
 #check out the pins package to cache and load the model
-load("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/init/Fit_3_States.RData")
-#mean and standard deviation are obtained from the training data
-#mean and SD 
-u <- -29764.73
-sd <- 463.4977
+#Create the task schedule job
+library(taskscheduleR)
+myscript <- ("D:/Documents/One Drive/OneDrive - Alexandria University/1- Ingest New Readings.R")
+taskscheduler_create(taskname = "Ingest-New-Readings", rscript = myscript,
+                     schedule = "MINUTE", starttime = "13:46", modifier = 1)
+
+#Check all tasks
+taskscheduler_ls()
+
+#delete the task
+taskscheduler_delete(taskname = "Ingest-New-Readings")
 
 init_file_list <- character(0)
-Results <- NULL
+Results <- data.frame(Results=numeric(), stringsAsFactors = F)
 setwd("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/init")
 save(init_file_list, file = "init_file_list.RData")
-write.csv(Results, file="Results.csv")
+save(Results, file="Results.RData")
