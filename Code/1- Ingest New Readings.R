5d69235584cf33b567f4294e0194d673f15bbb50
AkthemRehab <akthem@gmail.com>
2021-06-04 18:20:05 +0200

    sarted relying on the pins in the github repo. Remains the model which will be done locally untill it is properly butchered

--- Code/1- Ingest New Readings.R
+++ Code/1- Ingest New Readings.R
@@ -1,6 +1,7 @@
 library(rlist)
 
-load("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/init/init_file_list.RData")
+init_file_list <- pin_get("init_file_list", board = "GitHub-BFDD")
+Results <- pin_get("Results", board = "GitHub-BFDD")
 setwd("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/Data test")
 
 file_list <- list.files(pattern = "*.*")
@@ -13,8 +14,7 @@ if(!identical(setdiff(file_list, init_file_list),character(0))){
      test_1st_binded <- mapply(cbind, test_1st, "ID"=ID, SIMPLIFY=F)
      test_1st_binded <- list.rbind(test_1st_binded)
      init_file_list <- file_list
-     setwd("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/init")
-     save(init_file_list, file = "init_file_list.RData")
+     pin(init_file_list, board = "GitHub-BFDD")
      source("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/Code/2- Transform - Overlapping Windows.R")
 } else
      print("no new files found")
