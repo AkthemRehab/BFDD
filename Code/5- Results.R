a8c47001a0b99c53a6fce1c5ab5086d9b3d133cb
AkthemRehab <akthem@gmail.com>
2021-06-05 02:18:59 +0200

    Amended the loop to look at the vector not the list

--- Code/5- Results.R
+++ Code/5- Results.R
@@ -8,7 +8,7 @@ library(depmixS4)
 load("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/init/Fit_3_States.RData")
 Results <- pin_get("Results", board = "GitHub-BFDD")
 
-for(n in 1:length(temp)){
+for(n in 1:length(temp$file_list)){
      online_range <- (1+((n-1)*625)):(n*625)
      States_3_OM <- depmix(list(V_1_PC1~1, V_1_PC2~1, V_1_PC3~1, V_1_PC4~1, V_1_PC5~1, V_1_PC6~1, V_1_PC7~1, V_1_PC8~1, 
                                 V_1_PC9~1, V_1_PC10~1, V_2_PC1~1, V_2_PC2~1, V_2_PC3~1, V_2_PC4~1, V_2_PC5~1, 
