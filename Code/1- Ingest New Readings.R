5d1ea9fda4b0c8e99b1835d61fbce4fe8ac82f91
AkthemRehab <akthem@gmail.com>
2021-06-05 02:18:30 +0200

    amended the temp object, to be able to to the setdiff on data.frames and then return it to a vector object

--- Code/1- Ingest New Readings.R
+++ Code/1- Ingest New Readings.R
@@ -9,14 +9,14 @@ setwd("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/
 file_list <- data.frame(file_list=list.files(pattern = "*.*"))
 
 if(!identical(dim(setdiff(file_list, init_file_list))[1],as.integer(0))){
-     temp <- setdiff(file_list, init_file_list)
-     test_1st <- lapply(temp, read.delim, header = F, colClasses = c("NULL", "NULL", "NULL", "NULL", "NULL", 
+     temp <- c(setdiff(file_list, init_file_list))
+     test_1st <- lapply(temp$file_list, read.delim, header = F, colClasses = c("NULL", "NULL", "NULL", "NULL", "NULL", 
                                                                      "NULL", NA, NA))
      ID = c(1:length(test_1st))
      test_1st_binded <- mapply(cbind, test_1st, "ID"=ID, SIMPLIFY=F)
      test_1st_binded <- list.rbind(test_1st_binded)
      init_file_list <- file_list
-     pin(init_file_list, "init-file-list",board = "GitHub-BFDD")
+     pin(init_file_list, "init-file-list", board = "GitHub-BFDD")
      source("D:/Documents/One Drive/OneDrive - Alexandria University/IMS Bearing/BFDD/Code/2- Transform - Overlapping Windows.R")
 } else
      print("no new files found")
